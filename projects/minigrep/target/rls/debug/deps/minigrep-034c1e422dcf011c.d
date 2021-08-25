a16ec245a3bcfc79915d21fbc0c33e419523dec9
kainesmichea <keinesmicheal@gmail.com>
2021-08-25 09:47:43 +0000

    updated projects

--- projects/minigrep/target/rls/debug/deps/minigrep-034c1e422dcf011c.d
+++
@@ -1,5 +0,0 @@
-/home/micheal/Rust/projects/minigrep/target/rls/debug/deps/minigrep-034c1e422dcf011c.rmeta: src/lib.rs
-
-/home/micheal/Rust/projects/minigrep/target/rls/debug/deps/minigrep-034c1e422dcf011c.d: src/lib.rs
-
-src/lib.rs:
--- projects/minigrep/target/rls/debug/deps/minigrep-4fb4416ffaba41f5.d
+++
@@ -1,5 +0,0 @@
-/home/micheal/Rust/projects/minigrep/target/rls/debug/deps/minigrep-4fb4416ffaba41f5.rmeta: src/lib.rs
-
-/home/micheal/Rust/projects/minigrep/target/rls/debug/deps/minigrep-4fb4416ffaba41f5.d: src/lib.rs
-
-src/lib.rs:
